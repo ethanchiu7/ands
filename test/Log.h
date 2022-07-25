4d9a441f8ed2f54df73c6ba293eed77f53819de7
ethan <ethan>
2022-07-25 22:01:38 +0800

    add test main

---
+++ test/Log.h
@@ -0,0 +1,8 @@
+#ifndef Log_h
+#define Log_h
+
+#include <iostream>
+
+void Log(const char* message);
+
+#endif /* Log_h */
---
+++ test/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Log.h"
+using namespace std;
+
+int main(void) {
+    cout << "Hello Ethan" << endl;
+    Log("this is Log");
+
+
+}
